9c0232e2b8031a4f38b7d62c9ca75d9aec530f5a
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-07-23 13:50:08 -0700

    prod grunt build works

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -2,24 +2,13 @@ module.exports = function(grunt) {
 
     grunt.config.set('sass', {
         dev: {
-            options: {
-                style: 'expanded'
-            },
-            files: [
-                {
-                    expand: true,
-                    cwd: 'assets/styles/',
-                    src: ['*.scss', '*.sass'],
-                    dest: '.tmp/public/styles/',
-                    ext: '.css'
-                }, {
-                    expand: true,
-                    cwd: 'assets/vendor/',
-                    src: ['**/*.scss', '**/*.sass'],
-                    dest: '.tmp/public/vendor/',
-                    ext: '.css'
-                }
-            ]
+            files: [{
+                expand: true,
+                cwd: 'assets/styles/',
+                src: ['importer.scss'],
+                dest: '.tmp/public/styles/',
+                ext: '.css'
+            }]
         }
     });
 
--- tasks/register/prod.js
+++ tasks/register/prod.js
@@ -2,7 +2,20 @@ module.exports = function (grunt) {
 	grunt.registerTask('prod', [
 		'compileAssets',
 		'concat',
-		// 'sass:dev',
+		'sass:dev',
+		'uglify',
+		'cssmin',
+		'sails-linker:prodJs',
+		'sails-linker:prodStyles',
+		'sails-linker:devTpl',
+		'sails-linker:prodJsJade',
+		'sails-linker:prodStylesJade',
+		'sails-linker:devTplJade'
+	]);
+	grunt.registerTask('heroku:production', [
+		'compileAssets',
+		'concat',
+		'sass:dev',
 		'uglify',
 		'cssmin',
 		'sails-linker:prodJs',
