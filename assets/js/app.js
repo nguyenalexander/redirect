6faa8e96a2f6bd55d300fb9b2a155685017f9e75
Alexander Nguyen <nguyen.h.alexander@gmail.com>
2015-07-28 14:20:10 -0700

    added timer functionality with new url parameter

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,7 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     controller: 'MainCtrl',
     templateUrl: '../views/main.html'
   })
-  .when('/redirect/:url*/:title', {
+  .when('/redirect/:url*/:title/:id', {
     controller: 'InfoCtrl',
     templateUrl: '../views/form.html'
   })
@@ -23,7 +23,7 @@ app.controller('MainCtrl', ['$scope', function($scope){
 
 }]);
 
-app.controller('InfoCtrl', ['$scope', '$routeParams', '$window', '$http', '$sce', function($scope, $routeParams, $window, $http, $sce){
+app.controller('InfoCtrl', ['$scope', '$routeParams', '$window', '$http', '$sce', '$interval', function($scope, $routeParams, $window, $http, $sce, $interval){
 
   console.log('infoCtrl loaded')
 
@@ -31,13 +31,70 @@ app.controller('InfoCtrl', ['$scope', '$routeParams', '$window', '$http', '$sce'
     return ($scope.name && $scope.email && $scope.company) !== undefined;
   }
 
+  $scope.isValidTimer = function(){
+    if ($scope.timeOut !== 0) {
+      if($scope.name && $scope.email && $scope.company) {
+        $scope.timeOut = 0;
+        return ($scope.name && $scope.email && $scope.company) !== undefined;
+      }
+    }else if ($scope.timeOut == 0) {
+      return true;
+    }
+  }
+
   $scope.url = $routeParams.url
   $scope.title = $routeParams.title
 
-  console.log($scope.url, $scope.title)
+  console.log($scope.url, $scope.title, $routeParams.id)
+
+  $scope.timeOut = 10;
+  $scope.timerWatch = true;
+
+  var setTimer = function() {
+    if ($routeParams.id == 'a') {
+      $scope.requiredFields = true;
+      $scope.showTimer = false;
+      $scope.timeOut = false;
+    }else if ($routeParams.id == 'b') {
+      $scope.requiredFields = false;
+      $scope.showTimer = true;
+      timer = $interval(function() {
+        if ($scope.timeOut > 0){
+          $scope.timeOut -= 1;
+          return ($scope.timeOut)
+        }else if ($scope.timeOut == 0){
+          $scope.timerWatch = false
+          console.log('cancel', $scope.timeOut, $scope.timerWatch)
+          $interval.cancel(timer)
+        }
+      }, 1000)
+    }
+  }
+
+  setTimer();
+
+
+  $scope.$watch('timeOut', function(){
+    console.log('scope.timeOut has changed to', $scope.timeOut, $scope.timerWatch)
+    if($scope.timeOut > 1) {
+      $scope.message = 'Please wait '+$scope.timeOut+' more seconds to proceed.'
+    }else if ($scope.timeOut == 1) {
+      $scope.message = 'Please wait '+$scope.timeOut+' more second to proceed.'
+    }else if ($scope.timeOut == 0) {
+      $scope.timerWatch = false;
+      $scope.message = 'Submit!'
+    }
+    else if($scope.showTimer == false) {
+      $scope.timerWatch == false;
+      $scope.message = 'Submit!'
+    }
+  })
+
+
+
 
   $scope.submit = function(url) {
-    $http.post('/user', {name: $scope.name, email: $scope.email, company: $scope.company}).success(function(user){
+    $http.post('/user', {name: $scope.name, email: $scope.email, company: $scope.company, timer: $scope.showTimer}).success(function(user){
       console.log(user)
       if (($scope.url).indexOf('http://') == -1) {
         $window.location.href = 'http://' + url;
